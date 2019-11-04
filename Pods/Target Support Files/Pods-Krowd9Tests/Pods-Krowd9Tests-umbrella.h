8e8c7cae7e5fd339ea67541fee6db912bfe660c5
Javid Sheikh <javid@krowd9.com>
2019-11-04 10:11:30 +0000

    Add pods repo to version control

---
+++ Pods/Target Support Files/Pods-Krowd9Tests/Pods-Krowd9Tests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Krowd9TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Krowd9TestsVersionString[];
+
